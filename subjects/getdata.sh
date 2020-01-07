f233af6abe0bf5f50a97b2785dfdaca6be4d4844
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2020-01-07 09:34:43 +0900

    add getdata.sh

---
+++ subjects/getdata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Download OAS2.zip. The filesize is 11GB, so please be patient."
+curl -O http://www.md.tsukuba.ac.jp/clinical-med/psy-neuroimaging/nisg-202001/OAS2.zip
+
+unzip OAS2.zip
+
+echo "Done."
+
