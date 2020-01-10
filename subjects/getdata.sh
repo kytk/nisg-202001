b66d689a148027a9f10cefba4d5a9d7a3cac912a
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2020-01-10 19:08:39 +0900

    Update getdata.sh

--- subjects/getdata.sh
+++ subjects/getdata.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Download OAS2.zip. The filesize is 11GB, so please be patient."
-curl -O http://www.md.tsukuba.ac.jp/clinical-med/psy-neuroimaging/nisg-202001/OAS2.zip
+if [ ! -e OAS2.zip ]; then
+  echo "Download OAS2.zip. The filesize is 11GB, so please be patient."
+  curl -O http://www.md.tsukuba.ac.jp/clinical-med/psy-neuroimaging/nisg-202001/OAS2.zip
+fi
 
 unzip OAS2.zip
 
